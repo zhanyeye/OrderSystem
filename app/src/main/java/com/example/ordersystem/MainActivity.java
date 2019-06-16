a46fcdcbf049df58a4303a0cd98a1b40294d815f
zhanyeye <zhanyeye@qq.com>
2019-06-16 21:00:32 +0800

    抽屉布局

--- app/src/main/java/com/example/ordersystem/MainActivity.java
+++ app/src/main/java/com/example/ordersystem/MainActivity.java
@@ -3,24 +3,32 @@ package com.example.ordersystem;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavController.OnDestinationChangedListener{
+public class MainActivity extends AppCompatActivity implements NavController.OnDestinationChangedListener,NavigationView.OnNavigationItemSelectedListener {
 
     private NavController navController;
     private BottomNavigationView bottomNavigationView;
     public static MainActivity instance = null;
 
-
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,15 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
         navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
         navController.addOnDestinationChangedListener(this);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        // 显示左箭头
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -46,6 +63,17 @@ public class MainActivity extends AppCompatActivity implements NavController.OnD
         }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_camera:
+                Toast.makeText(this, "camera", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 
 }
